86a7b226de9d44e7fa9e38ecc2944fae11153c89
abhishrimal <60340221+abhishrimal@users.noreply.github.com>
2021-06-15 15:06:07 +0530

    QuestionAskedEmailDone

--- app/emailScript.js
+++ app/emailScript.js
@@ -20,13 +20,13 @@ function sendEmail() {
 }   
 
 /*Function to send Email for Volunteer Registration form.*/ 
-function sendVolunteerEmail() {
 
+function sendVolunteerEmail(){
     var name=document.querySelector('.form-control-m-215').value;
     var email=document.querySelector('.form-control-m-216').value;
     var phone=document.querySelector('.form-control-m-217').value;
     var details=document.querySelector('.form-control-m-219').value;
-    var e = document.getElementById("Volunteering-Option");
+    var e = document.getElementById("Category-Option");
     var strUser = e.value;
 
     Email.send({
@@ -39,5 +39,25 @@ function sendVolunteerEmail() {
 .then(function (message) {
   alert("Mail Sent Successfully")
 });
-    
-}      
+
+}  
+
+
+function sendAskQuestionEmail() {
+        var e = document.getElementById("Category-Option");
+        var strUserOption = e.value;
+        var name=document.getElementById('name').value;
+        var email=document.getElementById('email').value;
+        var message=document.getElementById('textmsg').value;
+        Email.send({
+        SecureToken: "b8908323-9876-4ca9-9c41-e13e8e4c569e",
+        To: email+",healthokseva@gmail.com",
+        From: "healthokseva@gmail.com",
+        Subject: 'Question Details sent to HealthOk',
+        Body: '<h2>Question Details sent to HealthOk:</h2> <br>Category-Option: '+strUserOption+'<br> Full Name: '+name+'<br> Email Id: '+email+'<br> Question Asked/Query: '+message, 
+    })
+    .then(function (message) {
+      alert("Your Question Sent Successfully")
+    });
+        
+    }    
